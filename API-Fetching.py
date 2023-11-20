c19177bf16185808b7cfc56216314ed8af2ccc38
MatthewMartin401 <129974886+MatthewMartin401@users.noreply.github.com>
2023-11-20 13:48:22 +0000

    Create API-Fetching.py
    Fetches API as promise. Converts promise to JSON Format. Parse data for variable, and outputs the variable.

---
+++ API-Fetching.py
@@ -0,0 +1,24 @@
+fun = async (url) => {  // Creates an async object function. Allowing the use of await. 
+    let response = await fetch(url) // await only used inside async function. Pauses function execution, awaiting promise. Fetch collected the data as a Promise from the URL.
+    let obj = await response.json()  // Converts to JSON format.
+    let data = obj.message
+    return data
+}
+
+let api_Url = ""  // API URL.
+fun(api_Url).then(
+    function(value){
+        console.log("Then: ", value)  // Execute if successful
+    },
+    function(error){
+        console.error("Then: ", error)  // Execute if unsuccessful
+    }
+)
+
+try{
+    fun(hello)
+    console.log("Try: ", "Done")  // Execute if successful
+}
+catch(error){
+    console.error("Catch: ", error)  // Execute if unsuccessful
+}
